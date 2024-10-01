c685419c446d8aeb286c747480516dd6a64bc69c
John <onyeiborjohn05@gmail.com>
2024-10-01 19:32:15 +0100

    try this

--- app/app/(dapp)/layout.tsx
+++ app/app/(dapp)/layout.tsx
@@ -5,8 +5,8 @@ const DAppLayout = ({ children }: { children: ReactNode }) => {
   return (
     <section className="flex flex-col items-center h-full relative overflow-hidden justify-between">
       {children}
-      <div className="absolute top-0 left-0 bg-[#F0A727] z-[-1] blur-[10000004.2150650024414px]  w-[171.194px] h-[168.041px]"></div>
-      <div className="absolute bottom-0 right-0 bg-[#F0A727] z-[-1] blur-[10000004.2150650024414px]  w-[171.194px] h-[168.041px]"></div>
+      <div className="absolute top-0 left-0 bg-[#F0A727] z-[-1]  w-[171.194px] h-[168.041px]"></div>
+      <div className="absolute bottom-0 right-0 bg-[#F0A727] z-[-1]   w-[171.194px] h-[168.041px]"></div>
       <Footer />
     </section>
   );
